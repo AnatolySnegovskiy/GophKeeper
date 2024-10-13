48b4eae235ba74b5996b7a2bb2f5ca8f8e8a9f6d
a.snegovskii <CarrionGrow@Gmail.com>
2024-10-13 15:39:15 +0300

    test

--- internal/client/ui/app_menu_test.go
+++ internal/client/ui/app_menu_test.go
@@ -1,6 +1,7 @@
 package ui
 
 import (
+	"github.com/gdamore/tcell/v2"
 	"testing"
 
 	"github.com/rivo/tview"
@@ -8,11 +9,38 @@ import (
 )
 
 func TestShowAppMenu(t *testing.T) {
+	// Create a test menu
 	menu := &Menu{
 		app:   tview.NewApplication(),
 		title: "Test Title",
 	}
 
+	// Call the showAppMenu function
 	menu.showAppMenu()
+
+	// Check that the app is not nil
 	assert.NotNil(t, menu.app)
+
+	list := tview.NewList().
+		AddItem("1. Файлы", "", '1', func() {
+			menu.showFilesMenu()
+		}).
+		AddItem("2. Пароли", "", '2', func() {
+			menu.showPasswordMenu()
+		}).
+		AddItem("3. Карты", "", '2', func() {
+			menu.showCardsMenu()
+		}).
+		SetSelectedFocusOnly(true)
+	// Check that the focus is set to the specific list created in showAppMenu
+	focused := menu.app.GetFocus()
+	assert.Equal(t, list, focused, "expected focus to be on the menu.list")
+
+	// Check that the input capture is set
+	assert.NotNil(t, menu.app.GetInputCapture())
+
+	// Check that the Esc key stops the app
+	event := tcell.NewEventKey(tcell.KeyEsc, 0, 0)
+	capturedEvent := menu.app.GetInputCapture()(event)
+	assert.Nil(t, capturedEvent, "expected capturedEvent to be nil")
 }
