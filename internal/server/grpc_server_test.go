d4d44da3d084b971c8dd1520b06971b6fdd028cc
a.snegovskii <CarrionGrow@Gmail.com>
2024-11-04 19:41:19 +0300

    add test

--- internal/server/grpc_server_test.go
+++ internal/server/grpc_server_test.go
@@ -5,15 +5,43 @@ import (
 	"github.com/DATA-DOG/go-sqlmock"
 	"github.com/redis/go-redis/v9"
 	"github.com/stretchr/testify/assert"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 	"goph_keeper/internal/server/services/jwt"
 	v1 "goph_keeper/internal/services/grpc/goph_keeper/v1"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 	"gorm.io/gorm/logger"
 	"log/slog"
+	"net"
 	"regexp"
 	"testing"
+	"time"
 )
+
+func TestGrpcServer_Run(t *testing.T) {
+	db, _, err := sqlmock.New()
+	assert.NoError(t, err, "an error was not expected when opening a stub database connection")
+
+	lis, err := net.Listen("tcp", ":0")
+	assert.NoError(t, err, "an error was not expected when listening on a TCP port")
+
+	server := getServer(db)
+	assert.NotNil(t, server, "server should not be nil")
+
+	go func() {
+		err := server.Run(lis)
+		assert.NoError(t, err, "an error was not expected when running the server")
+	}()
+
+	// Wait for the server to start
+	time.Sleep(100 * time.Millisecond)
+
+	// Test that the server is listening
+	cc, err := grpc.NewClient(lis.Addr().String(), grpc.WithTransportCredentials(insecure.NewCredentials()))
+	assert.NoError(t, err, "an error was not expected when dialing the server")
+	defer cc.Close()
+}
 
 func TestGrpcServer_RegisterUser(t *testing.T) {
 	db, mock, err := sqlmock.New()
@@ -30,15 +58,7 @@ func TestGrpcServer_RegisterUser(t *testing.T) {
 		WillReturnRows(sqlmock.NewRows([]string{"id"}).AddRow(1))
 
 	mock.ExpectCommit()
-	gdb, _ := gorm.Open(postgres.New(postgres.Config{Conn: db}), &gorm.Config{})
-	gdb.Logger = gdb.Logger.LogMode(logger.Silent)
-
-	server := NewGrpcServer(
-		slog.Default(),
-		jwt.NewJwt(),
-		&redis.Client{},
-		gdb,
-	)
+	server := getServer(db)
 	req := &v1.RegisterUserRequest{
 		Username:  "test_user",
 		Password:  "test_password",
@@ -50,4 +70,16 @@ func TestGrpcServer_RegisterUser(t *testing.T) {
 
 	assert.NoError(t, err)
 	assert.Equal(t, true, resp.Success)
+}
+
+func getServer(db gorm.ConnPool) *GrpcServer {
+	gdb, _ := gorm.Open(postgres.New(postgres.Config{Conn: db}), &gorm.Config{})
+	gdb.Logger = gdb.Logger.LogMode(logger.Silent)
+
+	return NewGrpcServer(
+		slog.Default(),
+		jwt.NewJwt(),
+		&redis.Client{},
+		gdb,
+	)
 }
