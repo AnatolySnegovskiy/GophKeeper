d4e9470a41bf1a3c8eeb341c05f5782bda523f3e
a.snegovskii <CarrionGrow@Gmail.com>
2024-11-04 13:50:01 +0300

    test

--- internal/client/ui/auth_menu_test.go
+++ internal/client/ui/auth_menu_test.go
@@ -2,19 +2,80 @@ package ui
 
 import (
 	"github.com/gdamore/tcell/v2"
+	"github.com/golang/mock/gomock"
 	"github.com/rivo/tview"
 	"github.com/stretchr/testify/assert"
+	"goph_keeper/internal/client"
+	"goph_keeper/internal/mocks"
+	v1 "goph_keeper/internal/services/grpc/goph_keeper/v1"
+	"log/slog"
+	"os"
 	"testing"
 )
 
 func TestShowRegistrationForm(t *testing.T) {
+	login := "TEST2"
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	mockClient := mocks.NewMockGophKeeperV1ServiceClient(ctrl)
+	mockClient.EXPECT().RegisterUser(gomock.Any(), gomock.Any()).Return(&v1.RegisterUserResponse{
+		Success: true,
+	}, nil).AnyTimes()
+	grpcClient := client.NewGrpcClient(slog.New(slog.NewJSONHandler(os.Stdout, nil)), mockClient, login, login)
+
 	menu := &Menu{
-		app:   tview.NewApplication(),
-		title: "Test Title",
+		app:        tview.NewApplication(),
+		title:      "Test Title",
+		grpcClient: grpcClient,
 	}
 
 	menu.showRegistrationForm()
-	assert.NotNil(t, menu.app)
+
+	focused := menu.app.GetFocus()
+
+	// Функция для симуляции нажатия клавиши
+	simulateKeyPress := func(key tcell.Key, primitive tview.Primitive) {
+		handler := primitive.InputHandler()
+		event := tcell.NewEventKey(key, 0, 0)
+		handler(event, func(p tview.Primitive) {})
+	}
+
+	// Симулируем ввод данных в поля формы
+	inputUsername := "testuser"
+	inputPassword := "testpass"
+	inputFormHandler := focused.InputHandler()
+	// Симулируем ввод имени пользователя
+	for _, r := range inputUsername {
+		inputFormHandler(tcell.NewEventKey(tcell.KeyRune, r, 0), nil)
+	}
+	focused = menu.app.GetFocus()
+	simulateKeyPress(tcell.KeyTab, focused) // Перейти к следующему полю
+	inputFormHandler = focused.InputHandler()
+	// Симулируем ввод пароля
+	for _, r := range inputPassword {
+		inputFormHandler(tcell.NewEventKey(tcell.KeyRune, r, 0), nil)
+	}
+
+	simulateKeyPress(tcell.KeyTab, focused) // Перейти к кнопке Register
+	focused = menu.app.GetFocus()
+	assert.IsType(t, &tview.Button{}, focused, "expected focused to be a tview.Form, but got %T", focused)
+	assert.Equal(t, "Register", focused.(*tview.Button).GetLabel(), "expected focused to be a tview.Form, but got %T", focused)
+	// Симулируем нажатие кнопки Register
+	simulateKeyPress(tcell.KeyEnter, focused)
+	assert.True(t, true, "expected showAuthorizationForm to be called")
+
+	menu.showRegistrationForm()
+	focused = menu.app.GetFocus()
+	simulateKeyPress(tcell.KeyTab, focused) // Перейти к следующему полю
+	simulateKeyPress(tcell.KeyTab, focused) // Перейти к кнопке Register
+	simulateKeyPress(tcell.KeyTab, focused) // Перейти к кнопке Cancel
+	focused = menu.app.GetFocus()
+	assert.IsType(t, &tview.Button{}, focused, "expected focused to be a tview.Form, but got %T", focused)
+	assert.Equal(t, "Cancel", focused.(*tview.Button).GetLabel(), "expected focused to be a tview.Form, but got %T", focused)
+
+	simulateKeyPress(tcell.KeyEnter, focused)
+	assert.True(t, true, "expected ShowMainMenu to be called")
+	os.RemoveAll("./.ssh/")
 }
 
 func TestShowAuthorizationForm(t *testing.T) {
