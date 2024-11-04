71db122170b4e51fd9230162679ad87dd83b4831
a.snegovskii <CarrionGrow@Gmail.com>
2024-11-04 19:59:54 +0300

    add test

--- go.mod
+++ go.mod
@@ -26,6 +26,7 @@ require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/gdamore/encoding v1.0.0 // indirect
+	github.com/go-redis/redismock/v9 v9.2.0 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
 	github.com/jackc/pgx/v5 v5.6.0 // indirect
--- internal/server/grpc_server_test.go
+++ internal/server/grpc_server_test.go
@@ -3,8 +3,9 @@ package server
 import (
 	"context"
 	"github.com/DATA-DOG/go-sqlmock"
-	"github.com/redis/go-redis/v9"
+	"github.com/go-redis/redismock/v9"
 	"github.com/stretchr/testify/assert"
+	passwordhash "github.com/vzglad-smerti/password_hash"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 	"goph_keeper/internal/server/services/jwt"
@@ -14,6 +15,7 @@ import (
 	"gorm.io/gorm/logger"
 	"log/slog"
 	"net"
+	"os"
 	"regexp"
 	"testing"
 	"time"
@@ -35,7 +37,7 @@ func TestGrpcServer_Run(t *testing.T) {
 	}()
 
 	// Wait for the server to start
-	time.Sleep(100 * time.Millisecond)
+	time.Sleep(1000 * time.Millisecond)
 
 	// Test that the server is listening
 	cc, err := grpc.NewClient(lis.Addr().String(), grpc.WithTransportCredentials(insecure.NewCredentials()))
@@ -72,14 +74,65 @@ func TestGrpcServer_RegisterUser(t *testing.T) {
 	assert.Equal(t, true, resp.Success)
 }
 
+func TestGrpcServer_AuthenticateUser(t *testing.T) {
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+	}
+	cleanup, _ := setupTestFiles("test_user")
+
+	password, _ := passwordhash.Hash("test_password")
+	mock.ExpectQuery(regexp.QuoteMeta("SELECT * FROM \"users\" WHERE username = $1 AND \"users\".\"deleted_at\" IS NULL ORDER BY \"users\".\"id\" LIMIT $2")).
+		WithArgs("test_user", 1).
+		WillReturnRows(sqlmock.NewRows([]string{"id", "username", "password", "ssh_pub_key"}).AddRow(1, "test_user", password, "-----BEGIN PUBLIC KEY-----\nMIICIjANBgkqhkiG9w0BAQEFAAOCAg8AMIICCgKCAgEA3mo7QRfA8cFnKhPfQz2P\nsVFKbjI4F+KQY74skglPN3B3lfE73/k16me46R4EryjTkBo91H0hi0v1rQ6Fuy6A\nG/o9PyNhGSRLWxnwg84ltry3+CVQcrA4UXQBoRTSsv+tjobF5X+QZl3u63ZbVeUH\n12OfOMQhJcwTcJ3TCA2z++XFIFMCgUPa6E3Uy7XxA3Vz2Pk1MXmatjYRJxrdf4U6\nONdS92xbea0E49LS/ckTwDqSeWo/2Jd5KtYBFbiOBNZpsWDA7//mB8GNx1w+UBbo\nLuAJG9k2mATQIirbb1MSqMiWJrQqZIBf3trhgt7Zo3VoYaVvfrvGBU3yj6FugScf\n2bTtBsVnYQkTCutZn7vnVVaNx5MJyLug6o7/nPiyXMpZv4mcQBFwyJB35gUqbqp3\njx5yvsXi0Pi+8nNlNFdpN1Vrr66BYJ4QrV2NeaCvylmi0lvxdqwEJKlw0O3IEGlQ\nhFbgU8pSX9E10bbt7CUX4HYFIVOdXBVvoNig6PmWPORpLYQAZnOaWn0BuxwKl+LT\nX3Acj+zTSm3mJIqjG2R6skDnZX8akQWmAJhMo8Kw3qC6wn5ggF3FPwg+/ontNnIu\nhc2HYebtmgU3DzSeFz/kkL2SNaV5JRBgJb4/Q+mh3q1YbZVJMetvBikE/soXEmzi\nSfKk5jdKtLL3P9PndPiS+jECAwEAAQ==\n-----END PUBLIC KEY-----\n"))
+
+	server := getServer(db)
+	req := &v1.AuthenticateUserRequest{
+		Username: "test_user",
+		Password: "test_password",
+	}
+
+	ctx := context.Background()
+	resp, err := server.AuthenticateUser(ctx, req)
+
+	assert.NoError(t, err)
+	assert.NotNil(t, resp)
+	assert.NotEmpty(t, resp.Token)
+	assert.Equal(t, true, resp.Success)
+	cleanup()
+}
+
 func getServer(db gorm.ConnPool) *GrpcServer {
 	gdb, _ := gorm.Open(postgres.New(postgres.Config{Conn: db}), &gorm.Config{})
 	gdb.Logger = gdb.Logger.LogMode(logger.Silent)
-
+	mockRedis, _ := redismock.NewClientMock()
 	return NewGrpcServer(
 		slog.Default(),
 		jwt.NewJwt(),
-		&redis.Client{},
+		mockRedis,
 		gdb,
 	)
+}
+
+func setupTestFiles(login string) (cleanup func(), err error) {
+	dir := "./.ssh/" + login
+	if err := os.MkdirAll(dir, 0700); err != nil {
+		return nil, err
+	}
+
+	publicKeyPath := dir + "/public_key.pem"
+	privateKeyPath := dir + "/private_key.pem"
+
+	if err := os.WriteFile(publicKeyPath, []byte("public_key_content"), 0600); err != nil {
+		return nil, err
+	}
+	if err := os.WriteFile(privateKeyPath, []byte("private_key_content"), 0600); err != nil {
+		return nil, err
+	}
+
+	cleanup = func() {
+		os.RemoveAll("./.ssh/" + login)
+	}
+
+	return cleanup, nil
 }
