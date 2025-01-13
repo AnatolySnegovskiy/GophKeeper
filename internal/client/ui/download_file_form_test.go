51e892c9e7573c1aa862a877b8e17581615581c8
a.snegovskii <CarrionGrow@Gmail.com>
2025-01-13 15:42:59 +0300

    more tests

--- internal/client/ui/download_file_form_test.go
+++ internal/client/ui/download_file_form_test.go
@@ -1,21 +1,19 @@
 package ui
 
 import (
-	"github.com/rivo/tview"
 	"github.com/stretchr/testify/assert"
 	v1 "goph_keeper/internal/services/grpc/goph_keeper/v1"
 	"testing"
 )
 
 func TestShowDownloadFileForm(t *testing.T) {
-	menu := &Menu{
-		app:   tview.NewApplication(),
-		title: "Test Title",
-	}
+	mockClient := getMockGRPCClient(t)
+	menu := getMenu(mockClient)
 
 	menu.showDownloadFileForm(&v1.ListDataEntry{
 		UserPath: "Test",
 		Uuid:     "Test",
 	}, func() {})
 	assert.NotNil(t, menu.app)
+	clear()
 }
