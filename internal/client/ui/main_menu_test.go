a6e3cf72f6060d8428e0f4e0662259faa37d3818
a.snegovskii <CarrionGrow@Gmail.com>
2025-01-13 21:25:59 +0300

    more tests

--- internal/client/ui/main_menu_test.go
+++ internal/client/ui/main_menu_test.go
@@ -1,6 +1,7 @@
 package ui
 
 import (
+	"github.com/gdamore/tcell/v2"
 	"github.com/rivo/tview"
 	"github.com/stretchr/testify/assert"
 	"testing"
@@ -14,4 +15,54 @@ func TestShowMainMenu(t *testing.T) {
 
 	menu.ShowMainMenu()
 	assert.NotNil(t, menu.app)
+}
+
+func TestSelectRegistration(t *testing.T) {
+	menu := &Menu{
+		app:   tview.NewApplication(),
+		title: "Test Title",
+	}
+
+	menu.ShowMainMenu()
+	assert.NotNil(t, menu.app)
+	focused := menu.app.GetFocus()
+	list, ok := focused.(*tview.List)
+	assert.True(t, ok, "focused should be of type *tview.List")
+
+	currentItemName, _ := list.GetItemText(list.GetCurrentItem())
+	assert.Equal(t, "1. Регистрация", currentItemName)
+	simulateKeyPress(tcell.KeyEnter, focused)
+}
+
+func TestSelectAuthorization(t *testing.T) {
+	menu := &Menu{
+		app:   tview.NewApplication(),
+		title: "Test Title",
+	}
+
+	menu.ShowMainMenu()
+	assert.NotNil(t, menu.app)
+	focused := menu.app.GetFocus()
+	list, ok := focused.(*tview.List)
+	assert.True(t, ok, "focused should be of type *tview.List")
+
+	simulateKeyPress(tcell.KeyDown, focused)
+
+	currentItemName, _ := list.GetItemText(list.GetCurrentItem())
+	assert.Equal(t, "2. Авторизация", currentItemName)
+	simulateKeyPress(tcell.KeyEnter, focused)
+}
+
+func TestEscape(t *testing.T) {
+	menu := &Menu{
+		app:   tview.NewApplication(),
+		title: "Test Title",
+	}
+
+	menu.ShowMainMenu()
+	assert.NotNil(t, menu.app)
+	focused := menu.app.GetFocus()
+	_, ok := focused.(*tview.List)
+	assert.True(t, ok, "focused should be of type *tview.List")
+	simulateKeyPress(tcell.KeyEscape, focused)
 }
