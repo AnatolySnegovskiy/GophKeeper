620b836f96b50c8be4838a454afebd164da92510
a.snegovskii <CarrionGrow@Gmail.com>
2025-01-17 23:20:56 +0300

    more terst

--- internal/client/ui/download_file_form_test.go
+++ internal/client/ui/download_file_form_test.go
@@ -1,6 +1,8 @@
 package ui
 
 import (
+	"fmt"
+	"github.com/gdamore/tcell/v2"
 	"github.com/golang/mock/gomock"
 	"github.com/rivo/tview"
 	"github.com/stretchr/testify/assert"
@@ -13,13 +15,40 @@ import (
 
 func TestShowDownloadFileForm(t *testing.T) {
 	mockClient := getMockGRPCClient(t)
-	menu := getMenu(mockClient)
+	testFile := getTestGoodFile()
+	mockStream := getDownloadStreaming(testFile, v1.Status_STATUS_SUCCESS)
+	mockClient.EXPECT().DownloadFile(gomock.Any(), gomock.Any()).Return(mockStream, nil)
+	mockClient.EXPECT().GetMetadataFile(gomock.Any(), gomock.Any()).Return(
+		&v1.GetMetadataFileResponse{
+			Metadata: "{\"file_name\":\"SynthVoiceRu.pak\",\"file_extension\":\".pak\",\"mem_type\":\"application/octet-stream\",\"is_compressed\":false,\"compression_type\":\"\",\"file_size\":2242646908}",
+		},
+		nil,
+	).AnyTimes()
 
+	menu := getMenu(mockClient)
 	menu.showDownloadFileForm(&v1.ListDataEntry{
-		UserPath: "Test",
-		Uuid:     "Test",
+		UserPath: "",
+		Uuid:     "1111",
 	}, func() {})
 	assert.NotNil(t, menu.app)
+
+	focused := menu.app.GetFocus()
+	_, ok := focused.(*tview.List)
+	assert.True(t, ok, "focused should be of type *tview.List")
+	simulateKeyPress(tcell.KeyDown, focused)
+	simulateKeyPress(tcell.KeyTab, focused)
+	focused = menu.app.GetFocus()
+	button, ok := focused.(*tview.Button)
+	assert.True(t, ok, "focused should be of type *tview.Button")
+	assert.Equal(t, "Select Directory", button.GetLabel())
+	simulateKeyPress(tcell.KeyEnter, focused)
+	focused = menu.app.GetFocus()
+
+	simulateKeyPress(tcell.KeyTab, focused)
+	focused = menu.app.GetFocus()
+	fmt.Printf("Focused widget type: %T\n", focused)
+
+	clear()
 }
 
 func TestCreateDownloadForm(t *testing.T) {
