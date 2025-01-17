97329ab2d82e1ef4002b8672ad527d4bbc0f3d33
a.snegovskii <CarrionGrow@Gmail.com>
2025-01-18 00:38:04 +0300

    more terst

--- internal/client/ui/exploer_test.go
+++ internal/client/ui/exploer_test.go
@@ -1,7 +1,9 @@
 package ui
 
 import (
+	"github.com/gdamore/tcell/v2"
 	"github.com/rivo/tview"
+	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
@@ -12,7 +14,52 @@ func TestExplore(t *testing.T) {
 	}
 	callback := func(filePath string, rollback func()) {
 	}
+	menu.explore(File, callback)
+	focused := menu.app.GetFocus()
+	list, ok := focused.(*tview.List)
+	assert.True(t, ok, "focused should be of type *tview.List")
+	simulateKeyPress(tcell.KeyUp, focused)
+	currentItemName, _ := list.GetItemText(list.GetCurrentItem())
+	assert.Equal(t, "Back", currentItemName)
+
+	simulateKeyPress(tcell.KeyEnter, focused)
+}
+
+func TestExploreUnix(t *testing.T) {
+	GetGOOS = func() string { return "linux" }
+	menu := &Menu{
+		app:   tview.NewApplication(),
+		title: "Test Title",
+	}
+	callback := func(filePath string, rollback func()) {
+	}
+	menu.explore(File, callback)
+	focused := menu.app.GetFocus()
+	list, ok := focused.(*tview.List)
+	assert.True(t, ok, "focused should be of type *tview.List")
+
+	currentItemName, _ := list.GetItemText(list.GetCurrentItem())
+	assert.Equal(t, "/", currentItemName)
+	simulateKeyPress(tcell.KeyEnter, focused)
+}
+
+func TestExploreDir(t *testing.T) {
+	GetGOOS = func() string { return "linux" }
+	menu := &Menu{
+		app:   tview.NewApplication(),
+		title: "Test Title",
+	}
+	callback := func(filePath string, rollback func()) {
+		assert.True(t, true)
+	}
 	menu.explore(Dir, callback)
+	focused := menu.app.GetFocus()
+	list, ok := focused.(*tview.List)
+	assert.True(t, ok, "focused should be of type *tview.List")
+
+	currentItemName, _ := list.GetItemText(list.GetCurrentItem())
+	assert.Equal(t, "/", currentItemName)
+	simulateKeyPress(tcell.KeyEnter, focused)
 }
 
 func TestShowDirectoryContents(t *testing.T) {
