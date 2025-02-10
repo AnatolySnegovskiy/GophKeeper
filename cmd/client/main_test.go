f446a0787e47af8f746f882f46d03e8a67e4a045
a.snegovskii <CarrionGrow@Gmail.com>
2025-02-10 20:23:28 +0300

    main test

--- cmd/client/main_test.go
+++ cmd/client/main_test.go
@@ -7,7 +7,7 @@ import (
 	"testing"
 )
 
-func TestRun(t *testing.T) {
+func TestMakeApp(t *testing.T) {
 	// Получаем текущую рабочую директорию
 	cwd, err := os.Getwd()
 	assert.NoError(t, err)
@@ -22,6 +22,30 @@ func TestRun(t *testing.T) {
 	configContent := `
 server:
   host: localhost
+  port: 8080
+`
+	err = os.WriteFile(configFile, []byte(configContent), 0644)
+	assert.NoError(t, err)
+	a := &app{}
+	a.makeApp()
+	os.RemoveAll(configDir)
+}
+
+func TestMakeAppFail(t *testing.T) {
+	// Получаем текущую рабочую директорию
+	cwd, err := os.Getwd()
+	assert.NoError(t, err)
+
+	// Создаем директорию для конфигурационного файла
+	configDir := filepath.Join(cwd, "configs")
+	err = os.MkdirAll(configDir, 0755)
+	assert.NoError(t, err)
+
+	// Создаем временный файл конфигурации для тестирования
+	configFile := filepath.Join(configDir, "config-client.yaml")
+	configContent := `
+server:
+  ho2st: localhost
   port: 8080
 `
 	err = os.WriteFile(configFile, []byte(configContent), 0644)
--- cmd/server/main_test.go
+++ cmd/server/main_test.go
@@ -10,7 +10,7 @@ import (
 	"testing"
 )
 
-func TestRun(t *testing.T) {
+func TestMakeApp(t *testing.T) {
 	// Получаем текущую рабочую директорию
 	cwd, err := os.Getwd()
 	assert.NoError(t, err)
@@ -30,6 +30,44 @@ redis:
   host: localhost
   port: 6379
   password: secret
+db:
+  dsn: postgres://user:password@localhost/dbname
+`
+	gormPostgres = func(dsn string) (*gorm.DB, error) {
+		return nil, nil
+	}
+	redisClient = func(opt *redis.Options) *redis.Client {
+		mockRedis, mock := redismock.NewClientMock()
+		mock.ExpectPing().SetVal("pong")
+		return mockRedis
+	}
+	err = os.WriteFile(configFile, []byte(configContent), 0644)
+	assert.NoError(t, err)
+	a := &app{}
+	a.makeApp()
+	os.RemoveAll(configDir)
+}
+
+func TestMakeAppFail(t *testing.T) {
+	// Получаем текущую рабочую директорию
+	cwd, err := os.Getwd()
+	assert.NoError(t, err)
+
+	// Создаем директорию для конфигурационного файла
+	configDir := filepath.Join(cwd, "configs")
+	err = os.MkdirAll(configDir, 0755)
+	assert.NoError(t, err)
+
+	// Создаем временный файл конфигурации для тестирования
+	configFile := filepath.Join(configDir, "config-server.yaml")
+	configContent := `
+server:
+  host: localhost
+  port: 5555
+redis:
+  host: localhost
+  por22t: 6379
+  pass2word: secret
 db:
   dsn: postgres://user:password@localhost/dbname
 `
