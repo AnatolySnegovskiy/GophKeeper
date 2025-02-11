77afafcd3269ae40a73bb6df9762b56a959031b4
a.snegovskii <CarrionGrow@Gmail.com>
2025-02-11 16:46:33 +0300

    main test

--- cmd/server/main_test.go
+++ cmd/server/main_test.go
@@ -47,39 +47,3 @@ db:
 	a.makeApp()
 	os.RemoveAll(configDir)
 }
-
-func TestMakeAppFail(t *testing.T) {
-	// Получаем текущую рабочую директорию
-	cwd, err := os.Getwd()
-	assert.NoError(t, err)
-
-	// Создаем директорию для конфигурационного файла
-	configDir := filepath.Join(cwd, "configs")
-	err = os.MkdirAll(configDir, 0755)
-	assert.NoError(t, err)
-
-	// Создаем временный файл конфигурации для тестирования
-	configFile := filepath.Join(configDir, "config-server.yaml")
-	configContent := `
-server
-redis:
-  host: localhost
-  por22t: 637
-  pass2word: secret
-db:
-  dsn: postgres://user:password@localhost/dbname
-`
-	gormPostgres = func(dsn string) (*gorm.DB, error) {
-		return nil, nil
-	}
-	redisClient = func(opt *redis.Options) *redis.Client {
-		mockRedis, mock := redismock.NewClientMock()
-		mock.ExpectPing().SetVal("pong")
-		return mockRedis
-	}
-	err = os.WriteFile(configFile, []byte(configContent), 0644)
-	assert.NoError(t, err)
-	a := &app{}
-	go a.makeApp()
-	os.RemoveAll(configDir)
-}
